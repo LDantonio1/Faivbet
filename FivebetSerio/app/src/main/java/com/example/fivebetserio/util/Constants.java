ae900e48db44ea024f3423d90e415a09af3c9f8b
LDantonio1 <l.dantonio1@campus.unimib.it>
2024-12-27 21:41:25 +0100

    implementazione roomDatabase

--- FivebetSerio/app/src/main/java/com/example/fivebetserio/util/Constants.java
+++ FivebetSerio/app/src/main/java/com/example/fivebetserio/util/Constants.java
@@ -11,5 +11,9 @@ public class Constants {
     //in questo array specifico (in modo statico) quali campionati mostrare
     //un potenziale sviluppo futuro è poter mostrare tutti i camoionati o far scegliere all'utente una lista di camoionati da vadere
     //lista completa: ("Primera División - Argentina", "A-League", "Belgium First Div", "Championship", "EFL Cup", "League 1", "League 2", "EPL", "FA Cup", "FIFA World Cup Winner", "Ligue 1 - France", "Ligue 2 - France", "Bundesliga - Germany", "Bundesliga 2 - Germany", "3. Liga - Germany", "Super League - Greece", "Serie A - Italy", "Serie B - Italy", "League of Ireland", "Liga MX", "Dutch Eredivisie", "Ekstraklasa - Poland", "Primeira Liga - Portugal", "La Liga - Spain", "La Liga 2 - Spain", "Premiership - Scotland", "Swiss Superleague", "Turkey Super League", "UEFA Europa Conference League", "UEFA Europa League")
-    public static final List<String> LEAGUES = Arrays.asList("Bundesliga - Germany", "Serie A - Italy");
+    public static final List<String> LEAGUES = Arrays.asList("Serie A - Italy");
+
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String SAVED_ARTICLES_DATABASE = "saved_db";
 }
